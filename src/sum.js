00830307a48777230edbcc726fb1981831337e95
Angelia Knight <aknight@ucvts.org>
2019-11-11 13:07:53 -0500

    initial comit

---
+++ src/sum.js
@@ -0,0 +1,7 @@
+const readline.Sync = require("readline-sync");
+
+const min = Number.MIN_SAFE_INTEGER;
+const max = Number.MAX_SAFE_INTEGER;
+
+const lowerBound = readlinesync.question("\nLower bound: ");
+const upperBound = readlinesync.question("\nUpper bound: ");
