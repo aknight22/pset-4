eb0c085cdeaecf7880ec92e7dbe641a2fe482673
Angelia Knight <aknight@ucvts.org>
2019-11-22 11:56:25 -0500

    okay

--- src/average.js
+++ src/average.js
@@ -4,8 +4,16 @@ const min = 0;
 const max = Number.MAX_SAFE_INTEGER;
 let number = 0;
 
+console.log();
+
+let sum = 0;
 do {
   number = Number(readlineSync.question("Non-negative integer: "));
-} while {
+  if (number >= 0 && number < max && !Number.isNaN(number) && Number.isInteger(number)) {
+    sum += number;
+  }
+} while (Number.isNaN(number) || (!Number.isInteger(number) || number >= 0));
 
-}
+
+
+ console.log(sum);
