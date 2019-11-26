1f947f2e6f1077c7621294e2e1ef5952b69243e7
aknight22 <44117063+aknight22@users.noreply.github.com>
2019-11-25 21:29:53 -0500

    last average

--- src/average.js
+++ src/average.js
@@ -18,6 +18,6 @@ do {
 
 let average = sum / count; 
 
-average = average.toLocaleString("en", { style: "decimal", maximumFractionDigits: 3, minimumFractionDigits: 3});
+average = average.toLocaleString("en", {maximumFractionDigits: 3, minimumFractionDigits: 3});
 
- console.log("/n" + average + ".");
+ console.log("\n" + average + ".");
